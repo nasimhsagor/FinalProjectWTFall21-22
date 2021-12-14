5d97c9503d00b088983e4026ee8639bee0d767a3
NASIM HAIDAR SAGOR <nasimhaidarsagor@gmail.com>
2021-12-15 03:36:38 +0600

    soikat & faruk vai er project add

--- control/header.php
+++ control/header.php
@@ -1,42 +1,42 @@
 <?php  
 
 switch ($_SERVER['SCRIPT_NAME']) {
-	case '/OFOS/view/login-form.php':
+	case 'localost/view/login-form.php':
 		 $CURRENT_PAGE = "Login";
 		 $PAGE_TITLE = "Login Page";
 		break;
 
-		case '/OFOS/view/registration-form.php':
+		case 'localost/view/registration-form.php':
 		 $CURRENT_PAGE = "Registration ";
 		 $PAGE_TITLE = "Registration  Page";
 		break;
 
-		case '/OFOS/view/home-page.php':
+		case 'localost/view/home-page.php':
 		 $CURRENT_PAGE = "Home";
 		 $PAGE_TITLE = "Home Page";
 		break;
 
-		case '/OFOS/view/profile.php':
+		case 'localost/view/profile.php':
 		 $CURRENT_PAGE = "Profile ";
 		 $PAGE_TITLE = "Profile Page";
 		break;
 
-		case '/OFOS/view/changepassword.php':
+		case 'localost/view/changepassword.php':
 		 $CURRENT_PAGE = "Change Password";
 		 $PAGE_TITLE = "Change Password Page";
 		break;
 
-		case '/OFOS/view/View-Menu.php':
+		case 'localost/View-Menu.php':
 		 $CURRENT_PAGE = "View-Menu";
 		 $PAGE_TITLE = "View-Menu Page";
 		break;
 
-		case '/OFOS/view/View-Order-Details.php':
+		case 'localost/View-Order-Details.php':
 		 $CURRENT_PAGE = "View-Order-Details ";
 		 $PAGE_TITLE = "View-Order-Details Page";
 		break;
 
-		case '/OFOS/view/Food-item.php':
+		case 'localost/Food-item.php':
 		 $CURRENT_PAGE = "Food Item ";
 		 $PAGE_TITLE = "Food Item Page";
 		break;
